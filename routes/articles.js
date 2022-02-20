3eb57f57c98a3a310892c2c1f1809e3a650dcc09
suti1979 <suti1979@gmail.com>
2022-02-20 10:02:35 +0100

    killed bug of null

--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,7 @@ const express = require("express")
 const Article = require("./../models/article")
 const router = express.Router()
 const { checkAuthenticated } = require("../config/auth")
+let article = null
 
 router.get("/new", checkAuthenticated, (req, res) => {
   res.render("articles/new", { article: new Article() })
